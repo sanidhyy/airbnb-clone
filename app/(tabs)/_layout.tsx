0aa5f4fb32881b0a4585fac5f2d0bcf92fd23141
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-18 09:56:13 +0000

    layout setup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const Layout = () => {
+  return <Tabs></Tabs>;
+};
+
+export default Layout;
