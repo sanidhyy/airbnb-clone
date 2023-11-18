80303834297c9043b3ecab68da0e329b254788e0
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-18 10:33:22 +0000

    path aliases added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
-import Colors from "../../constants/Colors";
+import Colors from "@/constants/Colors";
 
 const Layout = () => {
   return (
