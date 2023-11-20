a7ea85bfd7e8bd15250b700add044333fbfa3d4c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-20 12:11:06 +0000

    listings map created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,10 @@ import { Link, Stack } from "expo-router";
 
 import ExploreHeader from "@/components/ExploreHeader";
 import Listings from "@/components/Listings";
+import ListingsMap from "@/components/ListingsMap";
 
 import listingsData from "@/assets/data/airbnb-listings.json";
+import listingsDataGeo from "@/assets/data/airbnb-listings.geo.json";
 
 const Page = () => {
   const [category, setCategory] = useState("Tiny homes");
@@ -23,7 +25,8 @@ const Page = () => {
         }}
       />
 
-      <Listings listings={items} category={category} />
+      {/* <Listings listings={items} category={category} /> */}
+      <ListingsMap listings={listingsDataGeo} />
     </View>
   );
 };
