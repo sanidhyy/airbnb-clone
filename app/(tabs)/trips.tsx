05881a2b6461bb7b3e8f8aadfdf34d9c4d0ca69e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-20 15:49:29 +0000

    coming soon img added

--- app/(tabs)/trips.tsx
+++ app/(tabs)/trips.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Image, StyleSheet, Dimensions } from "react-native";
+
+const { height } = Dimensions.get("window");
 
 const Trips = () => {
   return (
-    <View>
-      <Text>Trips</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("@/assets/images/coming-soon.png")}
+        alt="Coming Soon"
+        style={{
+          height: 100,
+          width: 400,
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Trips;
